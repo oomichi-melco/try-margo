4c48cb829ce90f6411a9fcc97026628f84588bc3
Omichi Kenichi <Omichi.Kenichi@dn.MitsubishiElectric.co.jp>
2026-07-07 07:35:59 +0000

    Add coredns setting

--- try-margo/margo-installer/setup_margo_sandobox_client.sh
+++ try-margo/margo-installer/setup_margo_sandobox_client.sh
@@ -54,6 +54,39 @@ sudo cp ~/sandbox/scripts/harbor/certs/harbor.crt ~/certs
 set +ex
 
 echo "Changing coredns file.."
-kubectl -n kube-system get configmap coredns -o yaml
+set -ex
+pwd
+cd ../..
+kubectl -n kube-system get configmap coredns -o yaml > ./coredns.yaml
+patch -p0 < ./nodehosts.patch
+kubectl apply -f ./coredns.yaml
+kubectl -n kube-system rollout restart deployment coredns
+cd sandbox/scripts
+set +ex
+
+echo "Starting k3s agent.."
+export CI=false
+sudo -E bash device-agent.sh k3s start-k3s
+if [ $? -ne 0 ]; then
+	echo "Failed to start k3s agent."
+	exit 1
+fi
+
+echo "Checking k3s agent status.."
+sudo -E bash device-agent.sh k3s status
+if [ $? -ne 0 ]; then
+	echo "Failed to check k3s agent status."
+	exit 1
+fi
+
+kubectl -n default get pods
+
+echo "Installing otel.."
+# Fix this later.
+# sudo -E bash device-agent.sh k3s otel-install
+if [ $? -ne 0 ]; then
+	echo "Failed to install otel."
+	exit 1
+fi
 
 echo "Succeeded to run this script."
