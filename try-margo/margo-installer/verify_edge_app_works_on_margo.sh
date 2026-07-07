6302cf77900bece752dead75717bfd60f482a526
Omichi Kenichi <Omichi.Kenichi@dn.MitsubishiElectric.co.jp>
2026-07-07 15:15:54 +0000

    Rename container image

--- try-margo/margo-installer/verify_edge_app_works_on_margo.sh
+++ try-margo/margo-installer/verify_edge_app_works_on_margo.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname "$0")
 
-CONTAINER_IMAGE="oomichi-melco/edge-app:develop"
-HELM_IMAGE="oomichi-melco/edge-app-chart:1.0.0"
+CONTAINER_IMAGE="edge-app:develop"
+HELM_IMAGE="edge-app-chart:1.0.0"
 
 podman --version
 if [ $? -ne 0 ]; then
@@ -13,16 +13,10 @@ fi
 
 echo "Pushing container image to harbor.."
 set -ex
-docker pull ghcr.io/${CONTAINER_IMAGE}
-docker tag ghcr.io/${CONTAINER_IMAGE} harbor.machine:8443/${CONTAINER_IMAGE}
+docker pull ghcr.io/oomichi-melco/${CONTAINER_IMAGE}
+docker tag ghcr.io/oomichi-melco/${CONTAINER_IMAGE} harbor.machine:8443/library/${CONTAINER_IMAGE}
 docker login harbor.machine:8443 -u admin -p Harbor12345
-docker push harbor.machine:8443/${CONTAINER_IMAGE}
+docker push harbor.machine:8443/library/${CONTAINER_IMAGE}
 set +x
-
-echo "Pushing helm chart.."
-set -ex
-docker pull ghcr.io/${HELM_IMAGE}
-docker save -o helm.tar ghcr.io/${HELM_IMAGE}
-helm push helm.tar oci://harbor.machine:8443/library
 
 echo "Succeeded to run this script."
